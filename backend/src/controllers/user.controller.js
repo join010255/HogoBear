cca464bd3dbcebd310c17c1e3d50a462c282c5bf
join010255 <mohammedlelly2006@gmail.com>
2026-09-10 14:03:47 -0700

    update

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,1 +1,14 @@
-import crypto from "crypto";
+import UserService from "../services/user.service.js";
+
+
+class UserController {
+    async createAccount(httpReq, httpRes) {
+        return await UserService.createAccount(httpReq, httpRes);
+    }
+
+    async login(httpReq, httpRes) {
+        return await UserService.login(httpReq, httpRes);
+    }
+}
+
+export default new UserController();
---
+++ backend/src/middlewares/validation.middleware.js
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const validate = (schema) => (req, res, next) => {
+    try {
+        schema.parse({
+            body: req.body,
+            query: req.query,
+            params: req.params,
+        });
+        next();
+    } catch (err) {
+        return res.status(400).json({
+            message: "Validation error",
+            errors: err.errors,
+        });
+    }
+};
+
+export const createAccountSchema = z.object({
+    body: z.object({
+        password: z.string({
+            required_error: "Password is required",
+        }).min(6, "Password must be at least 6 characters long"),
+    }),
+});
+
+export const loginSchema = z.object({
+    body: z.object({
+        tokenUser: z.string({
+            required_error: "Token user is required",
+        }).regex(/^[a-f0-9]{64}$/i, "Invalid token format"),
+        password: z.string({
+            required_error: "Password is required",
+        }).min(1, "Password cannot be empty"),
+    }),
+});
